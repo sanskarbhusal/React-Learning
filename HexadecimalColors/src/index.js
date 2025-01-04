b5987eac3107a7225c45f92004150edcce7a602c
sanskarbhusal <101934318+sanskarbhusal@users.noreply.github.com>
2025-01-04 23:53:42 +0545

    Update index.js

--- HexadecimalColors/src/index.js
+++ HexadecimalColors/src/index.js
@@ -14,7 +14,6 @@ titleContainerStyle = {
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
-  backgroundColor: "red",
 };
 
 mainTitleStyle = {
@@ -24,6 +23,7 @@ mainTitleStyle = {
 subtitleStyle = {
   fontSize: "1.5em",
   marginTop: "8px",
+  marginBottom: "30px",
 };
 
 /*** Title Component ***/
@@ -36,21 +36,71 @@ const Title = ({ title: { mainTitle, subtitle } }) => {
   );
 };
 
+let gridStyle;
+let gridContainerStyle;
+
+gridContainerStyle = {
+  display: "flex",
+  flexWrap: "wrap",
+};
+
+gridStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "80px",
+  height: "80px",
+  margin: "1px",
+  color: "white",
+  fontWeight: "bolder",
+};
+
 const ColorGrid = () => {
+  function randomHex() {
+    const validHexChars = "abcdef1234567890";
+    const dict = validHexChars.split("");
+    let hex = "#";
+    for (let i = 0; i < 6; i++) {
+      const index = Math.floor(Math.random() * 16);
+      hex += dict[index];
+    }
+    return hex;
+  }
+  let gridArr = [];
+  for (let j = 0; j <= 31; j++) {
+    const color = randomHex();
+    gridArr.push(
+      <div key={j} style={{ ...gridStyle, backgroundColor: color }}>
+        {color}
+      </div>
+    );
+  }
   return (
     <div>
-      <div className="demo"></div>
-      <div className="demo"></div>
+      <div style={gridContainerStyle}>{gridArr}</div>
     </div>
   );
 };
+
+let appStyle;
+
+/*** CSS ***/
+appStyle = {
+  display: "flex",
+  flexDirection: "column",
+  width: "657px",
+  height: "420px",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
 const App = () => {
   const title = {
     mainTitle: "30 Days of React",
     subtitle: "Hexadecimal Colors",
   };
   return (
-    <div>
+    <div style={appStyle}>
       <Title title={title} />
       <ColorGrid />
     </div>
